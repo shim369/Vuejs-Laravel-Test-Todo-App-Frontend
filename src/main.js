ee7b2e688287cc3e720cfcb7186abc16d5a4c147
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-04-07 17:52:29 +0900

    add font awesome

--- src/main.js
+++ src/main.js
@@ -7,10 +7,11 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import {
     faEdit,
-    faTrash
+    faTrash,
+    faCheck
 } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faEdit, faTrash);
+library.add(faEdit, faTrash, faCheck);
 createApp(App)
 .use(routes)
 .component('font-awesome-icon', FontAwesomeIcon)
